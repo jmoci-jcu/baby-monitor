d6be72d1eb89fafd064b48fc8e6148f8eb97efa9
taylabender <tayla.bender@my.jcu.edu.au>
2025-07-31 15:26:08 +1000

    monitor audio level and log if sound exceeds the threshold

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <array>
 #include "WS2812.pio.h" // This header file gets produced during compilation from the WS2812.pio file
 #include "drivers/logging/logging.h"
+#include "drivers/microphone/microphone.h"
 
 #include "hardware_params.h"
 
@@ -13,12 +14,27 @@
 #include "IO/logger/logger.h"
 
 using namespace logger;
-      
+// threshold = 100.0f
+
 int main(){
-
-   stdio_init_all();
-   HumidityLevel humidityLevel = HumidityLevel(10);
-   log(humidityLevel);
-
-   return 0;
+    stdio_init_all();
+    
+    // Initialize microphone
+    mic_driver::init(4800);
+    
+    printf("Microphone detection started...\n");
+    
+    // Main loop
+    while (true) {
+         if (mic_driver::monitor_audio_level(100.0f)) { 
+            printf("Baby activity detected!\n");
+         }
+        sleep_ms(10);
+    }
+    
+    return 0;
 }
+
+
+
+
