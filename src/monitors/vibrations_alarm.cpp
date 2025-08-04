53d1bbcc3db64ec4b1dcc928ebc92a2cee9eca69
jmoci <jessemocilac@gmail.com>
2025-08-04 18:06:16 +1000

    integrate vibration sensor with logger

--- src/monitors/vibrations_alarm.cpp
+++ src/monitors/vibrations_alarm.cpp
@@ -7,6 +7,7 @@
 
 #include "pico/stdlib.h" // For stdio functions.   example: printf
 #include "drivers/accelerometer/accelerometer.h"
+#include "IO/logger/logger.h"
 
 // Internal state – only visible inside this file
 static absolute_time_t last_change_time = {0}; // Ensure proper initialization
@@ -15,31 +16,28 @@ static int16_t last_buffer[3] = {0}; // Ensure proper initialization
 
 void vibrations_alarm() {
     // Check if any axis changed significantly
-    //testing
-    uint16_t delta = 0;
     bool significant = false;
     int16_t g_buffer[3];
     read_axis(g_buffer);  // Reads latest accelerometer data
     // Check if any axis changed significantly
     for (int i = 0; i < 3; i++) {
-        if (std::abs(g_buffer[i] - last_buffer[i]) > 1000) {
-            delta = std::abs(g_buffer[i] - last_buffer[i]);
+        uint16_t delta = std::abs(g_buffer[i] - last_buffer[i]);
+        if (delta > 1000) {
             significant = true;
-            break;
         }
     }
 
-    if (significant) {
-        // Update the buffer after detecting movement
-        for (int i = 0; i < 3; i++) {
-            last_buffer[i] = g_buffer[i];
-        }
-        bool is_on_timeout = !(absolute_time_diff_us(last_change_time, get_absolute_time()) > 1 * 1000000);
-        if (!is_on_timeout) {
-            last_change_time = get_absolute_time();
-            printf("Movement detected! - Delta : %i\n",delta);
-        }
+    if(!significant){return;}
 
+    // Update the buffer after detecting movement
+    for (int i = 0; i < 3; i++) {
+        last_buffer[i] = g_buffer[i];
+    }
+    bool is_on_timeout = !(absolute_time_diff_us(last_change_time, get_absolute_time()) > 1 * 1000000);
+    if (!is_on_timeout) {
+        last_change_time = get_absolute_time();
+        Logger::VibrationLevelAlert vibrationLog = Logger::VibrationLevelAlert();
+        Logger::log(vibrationLog);
     }
 }
 
